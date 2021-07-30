de497fa1d8e82dc4ac9036ab33c774da493f5e6d
Tyler Rush <metarract@gmail.com>
2021-07-29 22:31:14 -0400

    changed map walker

--- src/MapGen.gd
+++ src/MapGen.gd
@@ -12,8 +12,8 @@ enum RoomEntrances {
 
 var roomArray = [];
 
-const MAX_MAP_WIDTH = 10
-const MAX_MAP_HEIGHT = 10
+export var MAX_MAP_WIDTH = 10
+export var MAX_MAP_HEIGHT = 10
 
 func _init():
   randomize()
@@ -25,10 +25,12 @@ func _init():
   print('Done')
 
 func initMap():
-  for _a in range(MAX_MAP_WIDTH):
+  for _x in range(MAX_MAP_WIDTH):
     var tempArray = []
-    for _b in range(MAX_MAP_HEIGHT):
+    for _y in range(MAX_MAP_HEIGHT):
       tempArray.push_back({
+        "x": _x,
+        "y": _y,
         "bits": 0,
         "room_id": -1
       })
@@ -50,44 +52,60 @@ func walkMap():
   # probe all nodes surrounding current node
   # return array of either OCCUPIED or UNOCCUPIED nodes
   # randomly index into array and make new node occupied
-  # A*? we should likeloy only move across and down
+  # we should likely only move across and down
 
   # once we reach final bottom node, push one MORE node into the same vertical array to make boss room
   # final bottom node will always be top down - maybe a rest area? something that denotes the boss is next
   # boss room also always top down
   #
   var initX = floor(randf() * roomArray.size())
-  var direction = 0
+  # var direction = 0
   var walk = Vector2(0,1)
   var coords = Vector2(initX,0)
   var currentRoom = roomArray[coords.x][coords.y]
   while coords.y < roomArray[0].size():
-    currentRoom.room_id = 0
-    var roomValid = false
-    var turnDirections = [-90,0,90]
-    while roomValid == false:
-      currentRoom = roomArray[coords.x][coords.y]
-      # randomly choose direction out of directions array
-      # if invalid splice it out of the array
-      var dirIndex = floor(randf() * turnDirections.size())
-      direction = deg2rad(turnDirections[dirIndex])
-      var tempWalk = walk.rotated(direction)
-      roomValid = isRoomValid()
-      if roomValid:
-        walk = tempWalk
-        break
-      else:
-        turnDirections.remove(dirIndex)
-        randomize()
-        pass
+    print(Vector2(currentRoom.x,currentRoom.y))
+    var validRooms = probeNodes(currentRoom)
+    print(validRooms)
+    randomize()
+    var index = floor(randf() * validRooms.size())
+    var r = validRooms[index]
+    currentRoom = roomArray[r.x][r.y]
+    # currentRoom.room_id = 0
+    # var roomValid = false
+    # var turnDirections = [-90,0,90]
+    # while roomValid == false:
+    #   currentRoom = roomArray[coords.x][coords.y]
+    #   # randomly choose direction out of directions array
+    #   # if invalid splice it out of the array
+    #   var dirIndex = floor(randf() * turnDirections.size())
+    #   direction = deg2rad(turnDirections[dirIndex])
+    #   var tempWalk = walk.rotated(direction)
+    #   roomValid = isRoomValid()
+    #   if roomValid:
+    #     walk = tempWalk
+    #     break
+    #   else:
+    #     turnDirections.remove(dirIndex)
+    #     randomize()
+    #     pass
     coords.x += walk.x
     coords.y += walk.y
 
-func isRoomValid() -> bool:
-  # if !currentRoom || currentRoom.room_id == 0:
-  #   pass
-  # pass
-  return true
+func probeNodes(currentRoom) -> Array:
+  var validRooms = []
+  pushRoomIfValid(validRooms, currentRoom.x-1, currentRoom.y)
+  pushRoomIfValid(validRooms, currentRoom.x+1, currentRoom.y)
+  pushRoomIfValid(validRooms, currentRoom.x, currentRoom.y+1)
+  return validRooms
+
+func pushRoomIfValid(validRooms: Array, x: int, y: int):
+  if x < 0 || x >= MAX_MAP_WIDTH-1 || y >= MAX_MAP_HEIGHT-1:
+    return
+  if roomArray[x][y].room_id != 0:
+    validRooms.push_back(Vector2(x,y))
+    pass
+  
 
 #var roomFound = false;
 #var retries = 0;
